030d4a85cd521f34804bded6b9de616f12e92ff0
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-08-16 15:39:41 -0400

    fixed player movement using vectors

--- assets/js/Objects/Bullet.js
+++ assets/js/Objects/Bullet.js
@@ -1,34 +1,32 @@
-import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/build/three.module.js';
+import * as THREE from "https://threejsfundamentals.org/threejs/resources/threejs/r127/build/three.module.js";
 class Bullet {
-    constructor(x, y, z, raycaster) {
-        this.x = x;
-        this.y = y; 
-        this.z = z;
-        this.size = .1;
-        this.speed = .5;
-        this.xSpeed = 0;
-        this.ySpeed = 0;
-        this.zSpeed = 0;
-        this.init(raycaster);
-    }
-    init(raycaster) {
-        this.geometry = new THREE.SphereGeometry(this.size, 32, 32);
-        this.material = new THREE.MeshPhongMaterial({ color: 0xff00ff });
-        this.mesh = new THREE.Mesh(this.geometry, this.material);
-        this.mesh.position.x = this.x;
-        this.mesh.position.y = this.y;
-        this.mesh.position.z = this.z;
-        this.xSpeed = raycaster.x * this.speed;
-        this.ySpeed = raycaster.y * this.speed;
-        this.zSpeed = raycaster.z * this.speed;
-    }
-    update() {
-        this.x += this.xSpeed;
-        this.y += this.ySpeed;
-        this.z += this.zSpeed;
-        this.mesh.position.x = this.x;
-        this.mesh.position.y = this.y;
-        this.mesh.position.z = this.z;
-    }
+  constructor(x, y, z, raycaster) {
+    this.pos = new THREE.Vector3(x, y, z);
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.size = 0.1;
+    this.speed = 0.5;
+    this.vel = raycaster.ray.direction.clone();
+    this.vel.multiplyScalar(this.speed);
+    this.xSpeed = 0;
+    this.ySpeed = 0;
+    this.zSpeed = 0;
+    this.init();
+  }
+  init() {
+    this.geometry = new THREE.SphereGeometry(this.size, 32, 32);
+    this.material = new THREE.MeshPhongMaterial({ color: 0xff00ff });
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.mesh.position.x = this.pos.x;
+    this.mesh.position.y = this.pos.y;
+    this.mesh.position.z = this.pos.z;
+  }
+  update() {
+    this.pos.add(this.vel);
+    this.mesh.position.x = this.pos.x;
+    this.mesh.position.y = this.pos.y;
+    this.mesh.position.z = this.pos.z;
+  }
 }
 export default Bullet;
